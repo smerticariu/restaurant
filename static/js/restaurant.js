a986c4dff7f65399d5f1000b033d80d1e28859a7
Sergiu <smerticariu@gmail.com>
2016-07-16 08:42:12 +0300

    add rating system

--- static/js/restaurant.js
+++ static/js/restaurant.js
@@ -1,27 +1,20 @@
-var restaurant = (function(){
-
-  setInterval(function(){
-    $(".success").fadeOut("slow");
-  }, 2000);
-
-});
+$(function() {
+    setInterval(function(){
+        $(".success").fadeOut("slow");
+    }, 2000);
 
 
+        $('.ratingjs2').html('<span class="stars">'+parseFloat($('.ratingjs').html())+'</span>');
+        $('span.stars').stars();
 
-    //   $('.logo').click(function() {
-    //     $('p').html('<span class="stars">'+parseFloat($('.raitingjs').val())+'</span>');
-    //     $('.ratingjs2').stars();
-    //   });
+            // $('input[type=submit]').click();
 
+$('.ratingjs2__client').html('<span class="stars">'+parseFloat($('.ratingjs__client').html())+'</span>');
+        //$('span.stars').stars();
 
-    // $.fn.stars = function() {
-    //   return $(this).each(function() {
-    //     $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * 16));
-    //   });
-    // }
-
-
-
-
-
-restaurant();
+        });
+$.fn.stars = function() {
+    return $(this).each(function() {
+        $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * 16));
+    });
+}
