f6fda2a3dcd9219722b03e427cd48f7312194fc2
Sergiu <smerticariu@gmail.com>
2016-07-17 21:42:05 +0300

    add rating system

--- static/js/restaurant.js
+++ static/js/restaurant.js
@@ -4,15 +4,11 @@ $(function() {
     }, 2000);
 
 
-        $('.ratingjs2').html('<span class="stars">'+parseFloat($('.ratingjs').html())+'</span>');
-        $('span.stars').stars();
+    $('.ratingjs2').html('<span class="stars">'+parseFloat($('.ratingjs').html())+'</span>');
+    $('span.stars').stars();
 
-            // $('input[type=submit]').click();
-
-$('.ratingjs2__client').html('<span class="stars">'+parseFloat($('.ratingjs__client').html())+'</span>');
-        //$('span.stars').stars();
-
-        });
+});
+// Rating system
 $.fn.stars = function() {
     return $(this).each(function() {
         $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * 16));
